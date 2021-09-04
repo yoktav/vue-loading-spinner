50e53a1c6103da8b0fb96283669720740bdc1da3
Yılmaz Oktav <hello@yilmazoktav.com>
2021-09-04 14:37:14 +0300

    Initial commit

---
+++ LoadingSpinner.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg class="c-loading-spinner" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+    <circle
+      class="c-loading-spinner__circle"
+      fill="none"
+      stroke-width="6"
+      stroke-linecap="round"
+      cx="33"
+      cy="33"
+      r="30"
+    ></circle>
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+@import './LoadingSpinner';
+</style>
---
+++ README.md
@@ -0,0 +1,1 @@
+# Vue.js Loading Spinner
